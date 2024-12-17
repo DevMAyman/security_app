39457a1ecdf3675426a2646063ed566daaed9438
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-12-17 22:06:20 +0200

    some beautiful formatting

--- src/main/java/com/security_app/config/ProjectSecurityConfig.java
+++ src/main/java/com/security_app/config/ProjectSecurityConfig.java
@@ -6,13 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -36,8 +33,4 @@ public class ProjectSecurityConfig {
     }
 
 
-//    @Bean
-//    public UserDetailsService userDetailsService(DataSource dataSource){
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
 }
